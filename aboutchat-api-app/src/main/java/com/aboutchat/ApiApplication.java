6ef351e1c8c3657332c2d54c0dd95505e6e2cb64
TunaJoeN <445136813@qq.com>
2023-03-01 22:41:16 +0800

    创建工程

---
+++ aboutchat-api-app/src/main/java/com/aboutchat/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.aboutchat;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author TunaJoe
+ * @Date2023/3/121:46
+ * @Description 启动类
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+
+    }
+}
